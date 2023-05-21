14e02df275a064769262494c8950a07a1806c763
hukh1998 <hukaitong1128@gmail.com>
2023-05-20 23:46:53 -0500

    Update BMI.py

--- BMI.py
+++ BMI.py
@@ -1,18 +1,18 @@
 import streamlit as st
 
-def add_bg_from_url():
-    st.markdown(
-         f"""
-         <style>
-         .stApp {{
-             background-image: url("https://bpb-us-w2.wpmucdn.com/voices.uchicago.edu/dist/e/2560/files/2019/04/UChicago_Phoenix-Wallpaper-Gray.jpg");
-             background-attachment: fixed;
-             background-size: cover
-         }}
-         </style>
-         """,
-         unsafe_allow_html=True
-     )
+# def add_bg_from_url():
+#     st.markdown(
+#          f"""
+#          <style>
+#          .stApp {{
+#              background-image: url("https://bpb-us-w2.wpmucdn.com/voices.uchicago.edu/dist/e/2560/files/2019/04/UChicago_Phoenix-Wallpaper-Gray.jpg");
+#              background-attachment: fixed;
+#              background-size: cover
+#          }}
+#          </style>
+#          """,
+#          unsafe_allow_html=True
+#      )
     
 
 st.set_page_config(
@@ -20,7 +20,7 @@ st.set_page_config(
     layout = "wide",
     initial_sidebar_state="expanded")
 
-add_bg_from_url() 
+#add_bg_from_url() 
 
 st.sidebar.success("Select a method above.")
 
